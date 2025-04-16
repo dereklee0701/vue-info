ac57d70a96e95a331a85b7a5d720507a46c2f978
dereklee0701 <dereklee1012@gmail.com>
2025-04-16 10:59:04 +0900

    Add files via upload

---
+++ vue2-basic/19.vue cli 기본구조/School.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="demo">
+        <h3>학교명 : {{name}}</h3>
+        <h3>학교주소 : {{addr}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'School',
+        data(){
+            return {
+                name:'서강대',
+                addr:'서울' 
+            }
+        }
+    }
+</script>
+
+<style>
+    .demo{
+        background-color: beige;
+    }
+</style>
---
+++ vue2-basic/19.vue cli 기본구조/Student.vue
@@ -0,0 +1,18 @@
+<template>
+    <div >
+        <h3>학생명 : {{name}}</h3>
+        <h3>학생나이 : {{age}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Student',
+        data(){
+            return {
+                name:'홍길동',
+                addr:23 
+            }
+        }
+    }
+</script>
